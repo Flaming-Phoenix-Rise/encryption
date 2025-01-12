b863bb5a2dd6494358b01ff2797abdabce6e7729
Flaming Phoenix <102559132+Flaming-Phoenix-Rise@users.noreply.github.com>
2025-01-11 20:14:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import os
 import tempfile
 from pymongo import MongoClient
 
+MONGO_URI = "mongodb+srv://andyduoduo8@gmail.com:u5GP4TV#yutiAnD@cluster22207.vyuec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster22207"
+    client = MongoClient(MONGO_URI)
+    db = client.Encryption
+    mycol = db.Keys
 BUFFER_SIZE = 64 * 1024
 
 def encrypt_file(uploaded_file, name, mycol):
@@ -72,10 +76,6 @@ def decrypt_file(uploaded_file, name, mycol, file_extension):
 
 def main():
     st.title("Encryption Tool")
-    mongodb+srv://andyduoduo8@gmail.com:u5GP4TV#yutiAnD@cluster22207.vyuec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster22207
-    client = MongoClient(MONGO_URI)
-    db = client.FaceRecognitionApp
-    users_collection = db.Users
     name = st.text_input("Enter Your Name:")
     if not name:
         st.warning("Please enter your name to proceed.")
