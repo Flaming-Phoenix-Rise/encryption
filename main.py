c377fd196cc80572f5bde949c8a7d58cf696ebc2
Flaming Phoenix <102559132+Flaming-Phoenix-Rise@users.noreply.github.com>
2025-01-12 14:50:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from pymongo.server_api import ServerApi
 MONGO_URI = "mongodb+srv://Phoenix:Gv68TLSM23qwENrB@project.oihvt.mongodb.net/?retryWrites=true&w=majority&appName=Project"
 client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 db = client["Encryption"]
-print(db.list_collection_names())
 mycol = db["Keys"]
 BUFFER_SIZE = 64 * 1024
 
